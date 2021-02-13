959b2e2c89af6140998d93fcca41bfc3235b4649
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2021-02-13 11:42:28 -0800

    Populated the quotes data

--- quotes.js
+++ quotes.js
@@ -11,7 +11,16 @@ const quotes = {
             {
                 title: "Guarantee Timmie an Education",
                 description: "Donate to the Kids in Need Foundation to provide children with the school supplies they need to flourish.",
-                donation_link: "https://www.kinf.org/donate/",
+                image_url: ""
+            },
+            {
+                title: "Provide Food for People Experiencing Homelessness",
+                description: "Donate to the National Alliance to End Homelessness to provide housing and crisis funds to the homeless in the United States.",
+                image_url: ""
+            },
+            {
+                title: "Save a Child’s Life",
+                description: "Donate to St. Jude Children’s Research Hospital to help children recover from unfortunate ailments.",
                 image_url: ""
             }
         ],
@@ -19,25 +28,21 @@ const quotes = {
             {
                 title: "UC Berkeley Hoodie",
                 description: "Such a waste of money, right?",
-                donation_link: "",
                 image_url: ""
             },
             {
                 title: "Among Us Skins",
                 description: "Gotta make sure your virtual avatar looks cute when you’re inevitably tossed into the vacuum of space.",
-                donation_link: "",
                 image_url: ""
             },
             {
                 title: "An Extra iPhone Headphones Dongle",
                 description: "Who doesn’t want their phone to look like an octopus?",
-                donation_link: "",
                 image_url: ""
             },
             {
                 title: "More Baking Supplies",
                 description: "One more batch of quarantine cookies won’t hurt, right?",
-                donation_link: "",
                 image_url: ""
             }
         ],
@@ -45,17 +50,35 @@ const quotes = {
     medium: {
         serious: [
             {
-                title: "",
-                description: "",
-                donation_link: "",
+                title: "Sponsor a Future US Citizenship",
+                description: "Donate to Citizenshipworks to help make applying for United States citizenship safer, simpler, and more affordable, and to bring the power of citizenship to more people.",
+                image_url: ""
+            },
+            {
+                title: "Adopt a Puppy or Kitten",
+                description: "Provide a forever home for an adorable new friend from the Humane Society or donate to support the organization’s efforts.",
+                image_url: ""
+            },
+            {
+                title: "Support Mental Health Initiatives",
+                description: "Donate to Mental Health America to help fund mental health therapy programs for children and adults in the United States.",
                 image_url: ""
             }
         ],
         silly: [
             {
-                title: "",
-                description: "",
-                donation_link: "",
+                title: "Invest in Gamestop Stock",
+                description: "With a peak price of $350, you’re sure to net a great return on your investment!",
+                image_url: ""
+            },
+            {
+                title: "500 Cup Noodles",
+                description: "Try all of their 100+ delicious flavors at least 5 times each!",
+                image_url: ""
+            },
+            {
+                title: "Upgrade Your Wifi",
+                description: "Make sure that your Netflix and Disney Plus accounts never decrease their streaming quality by ensuring that you have the best wifi possible.",
                 image_url: ""
             }
         ],
@@ -63,17 +86,30 @@ const quotes = {
     high: {
         serious: [
             {
-                title: "",
-                description: "",
-                donation_link: "",
+                title: "End COVID-19",
+                description: "Donate money to the National Foundation for Infectious Diseases to support vaccine research for deadly diseases, such as Covid-19, Polio, and more.",
+                image_url: ""
+            },
+            {
+                title: "End Deforestation in the Amazon",
+                description: "Stop hacking the trees and go to TreeHacks!",
+                image_url: ""
+            },
+            {
+                title: "Cleanup Oil Spills in the World",
+                description: "Support the International Spill Control Organization (ISCO) to raise worldwide preparedness to oil and chemical spills all over the world.",
                 image_url: ""
             }
         ],
         silly: [
             {
-                title: "",
-                description: "",
-                donation_link: "",
+                title: "A Semester of your Online College’s Tuition",
+                description: "Still paying full price to attend Zoom University?",
+                image_url: ""
+            },
+            {
+                title: "Sue the Electoral College",
+                description: "Lost an election recently? Go ahead and sue them!",
                 image_url: ""
             }
         ]
