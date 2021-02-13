5d8302052ee240f0f16dbe42a596d9a097917228
Amartya Ranganathan <amartyarr@gmail.com>
2021-02-13 15:34:25 -0800

    most recent front end

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
+var output = document.getElementById("donation");
 document.getElementById('buy').onclick = test;
 output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
-  output.innerHTML = this.value;
+  output.value = this.value;
 }
 
 function test(){
