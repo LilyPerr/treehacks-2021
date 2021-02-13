4f48a3bf4f964271d41de59bab006b91958f9f37
Amartya Ranganathan <amartyarr@gmail.com>
2021-02-13 14:10:57 -0800

    alerts

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
+document.getElementById('buy').onclick = test;
 output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
   output.innerHTML = this.value;
+}
+
+function test(){
+  alert('Are you really that heartless?');
 }
