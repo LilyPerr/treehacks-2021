48d92f7980657074b9def3ab3aa6e805beb6f005
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2021-02-13 09:03:12 -0800

    Created quotes object to store our quotes
    Make it a javascript object since we will only use it in our front end

---
+++ quotes.js
@@ -0,0 +1,81 @@
+/**
+ * List of quotes
+ * @type {{}}
+ */
+
+// Usage: quotes.low.serious[0].title
+
+const quotes = {
+    low: {
+        serious: [
+            {
+                title: "Guarantee Timmie an Education",
+                description: "Donate to the Kids in Need Foundation to provide children with the school supplies they need to flourish.",
+                donation_link: "https://www.kinf.org/donate/",
+                image_url: ""
+            }
+        ],
+        silly: [
+            {
+                title: "UC Berkeley Hoodie",
+                description: "Such a waste of money, right?",
+                donation_link: "",
+                image_url: ""
+            },
+            {
+                title: "Among Us Skins",
+                description: "Gotta make sure your virtual avatar looks cute when you’re inevitably tossed into the vacuum of space.",
+                donation_link: "",
+                image_url: ""
+            },
+            {
+                title: "An Extra iPhone Headphones Dongle",
+                description: "Who doesn’t want their phone to look like an octopus?",
+                donation_link: "",
+                image_url: ""
+            },
+            {
+                title: "More Baking Supplies",
+                description: "One more batch of quarantine cookies won’t hurt, right?",
+                donation_link: "",
+                image_url: ""
+            }
+        ],
+    },
+    medium: {
+        serious: [
+            {
+                title: "",
+                description: "",
+                donation_link: "",
+                image_url: ""
+            }
+        ],
+        silly: [
+            {
+                title: "",
+                description: "",
+                donation_link: "",
+                image_url: ""
+            }
+        ],
+    },
+    high: {
+        serious: [
+            {
+                title: "",
+                description: "",
+                donation_link: "",
+                image_url: ""
+            }
+        ],
+        silly: [
+            {
+                title: "",
+                description: "",
+                donation_link: "",
+                image_url: ""
+            }
+        ]
+    }
+}
