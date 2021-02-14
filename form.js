524096f8c83ddf53b04a87b44b56a7ee157fd3ca
Arthur Tham <19481191+arthurtham@users.noreply.github.com>
2021-02-13 20:52:44 -0800

    Added flask middleman to deal with checkbook api

--- form.js
+++ form.js
@@ -5,14 +5,14 @@ function checkbook(){
 	var name = document.getElementById("inputName");
 	var amount = document.getElementById("inputAmount");
 	var description = document.getElementById("inputDescription");
-	//console.log(email.value, name.value, amount.value, description.value);
+	console.log(email.value, name.value, amount.value, description.value);
 	const options = {
 	  method: 'POST',
-	  headers: {Accept: 'application/json', 'Content-Type': 'application/json', Authorization: 'f0d00dc100818adb44bd6b4533294b95:c2efd5fee1ae40139d654095884b76a7'},
-	  body: '{"recipient":"treebook@arttham.com","name":'+name.value+',"amount":'+amount.value+',"description":'+description.value+'}'
+	  //headers: {Accept: 'application/json', 'Content-Type': 'application/json'},
+	  body: '{"recipient":"treebook@arttham.com","name":\"'+name.value+'\","amount":'+amount.value+',"description":\"'+description.value+'\"}'
 	};
 
-	fetch('https://sandbox.checkbook.io/v3/check/digital', options)
+	fetch('https://a31849cfda11.ngrok.io/', options)
 	  .then(response => console.log(response))
 	  .catch(err => console.error(err));
 }
